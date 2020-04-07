8ffd839e530586cc3ce15ceaddb593e0466b527b
yosiassapari <yosiassapari@gmail.com>
2020-04-07 09:46:36 -0700

    seperating screens into each file

---
+++ lib/second_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'first_screen.dart';
+
+class SecondScreen extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Pet Plant'),
+          centerTitle: true,
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+//                Container(
+//                    alignment: Alignment.center,
+//                    padding: EdgeInsets.all(10),
+//                    child: Text(
+//                      'Sign in',
+//                      style: TextStyle(fontSize: 20),
+//                    )),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'User Name',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: nameController,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Location',
+                    ),
+                  ),
+                ),
+                Container(
+                    height: 50,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Register'),
+                      onPressed: () {
+                        print(nameController.text);
+                        print(passwordController.text);
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => FirstScreen()),
+                        );
+                      },
+                    )),
+              ],
+            )));
+  }
+}
---
+++ lib/third_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Pet Plant'),
+          centerTitle: true,
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(10),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Welcome user!',
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 30),
+                    )),
+                Image.asset(
+                  'assets/38911_HonaiTech_logo_J_01.png',
+                ),
+                Container(
+                    alignment: Alignment.center,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      'Your plant',
+                      style: TextStyle(fontSize: 20),
+                    )),
+                ListTile(
+                  title: Text('Overall heatlh                           : Good'),
+                ),
+                ListTile(
+                  title: Text('Moisture level                          : 60%'),
+                ),
+                ListTile(
+                  title: Text('Water level                                : 70%'),
+                ),
+                ListTile(
+                  title: Text('Temperature                            : 27 deg C'),
+                ),
+                Container(
+                    height: 40,
+                    padding: EdgeInsets.fromLTRB(10, 0, 10, 2.5),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Switch plant'),
+                      onPressed: () {
+                      },
+                    )),
+                Container(
+                    height: 40,
+                    padding: EdgeInsets.fromLTRB(10, 2.5, 10, 0),
+                    child: RaisedButton(
+                      textColor: Colors.white,
+                      color: Colors.blue,
+                      child: Text('Add another plant'),
+                      onPressed: () {
+                      },
+                    )),
+              ],
+            )));
+  }
+}
