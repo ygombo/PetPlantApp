b170219dcc1af13ccef999425bc74e49a0d85589
ygombo1016 <gombo.yoshua@yahoo.com>
2020-04-08 10:47:46 -0700

    Reviewed.

--- lib/main.dart
+++ lib/main.dart
@@ -7,29 +7,6 @@ void main() {
   ));
 }
 
-//class MyApp extends StatefulWidget {
-//  @override
-//  _State createState() => _State();
-//}
-
-//class FirstScreen extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: AppBar(
-//        title: Text('First Route'),
-//      ),
-//      body: Center(
-//        child: RaisedButton(
-//          child: Text('Open route'),
-//          onPressed: () {
-//            // Navigate to second route when tapped.
-//          },
-//        ),
-//      ),
-//    );
-//  }
-//}
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext ctxt) {
