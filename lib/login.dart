b52111af3535c1b2eb16b1464b3e1e60edbe89e8
yosiassapari <yosiassapari@gmail.com>
2020-04-10 10:47:39 -0700

    disabled the back button on the login menu in appbar

--- lib/login.dart
+++ lib/login.dart
@@ -1,8 +1,6 @@
-
 import 'package:flutter/material.dart';
 import 'package:petplant/home.dart';
 import 'package:petplant/signup.dart';
-
 
 class Login extends StatelessWidget {
   final nameController = TextEditingController();
@@ -11,110 +9,109 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Pet Plant'),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-        ),
-        body: Padding(
-            padding: EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                Image.asset(
-                  'assets/HT_logo.png',
-                  height: 200,
-                  width: 200,
-                ),
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Welcome!',
-                      style: TextStyle(
-                          color: Colors.green,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30),
-                    )),
-                Container(
+      appBar: AppBar(
+        title: Text('Pet Plant'),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        leading: Container(),
+      ),
+      body: Padding(
+          padding: EdgeInsets.all(10),
+          child: ListView(
+            children: <Widget>[
+              Image.asset(
+                'assets/HT_logo.png',
+                height: 200,
+                width: 200,
+              ),
+              Container(
+                  alignment: Alignment.center,
                   padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    style: TextStyle(color:Colors.black),
-                    decoration: InputDecoration(
+                  child: Text(
+                    'Welcome!',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: nameController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.black,
-                        )
-                      ),
+                          borderSide: BorderSide(
+                        color: Colors.black,
+                      )),
                       labelText: 'User Name',
-                      labelStyle: TextStyle(color:Colors.grey)
-                    ),
-                  ),
+                      labelStyle: TextStyle(color: Colors.grey)),
                 ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                      style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: passwordController,
+                  style: TextStyle(color: Colors.black),
+                  decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(
                           color: Colors.black,
                         ),
                       ),
                       labelText: 'Password',
-                      labelStyle: TextStyle(color: Colors.grey)
-
-                    ),
+                      labelStyle: TextStyle(color: Colors.grey)),
+                ),
+              ),
+              FlatButton(
+                onPressed: () {
+                  //forgot password screen
+                },
+                textColor: Colors.green,
+                child: Text('Forgot Password'),
+              ),
+              Container(
+                  height: 50,
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.green,
+                    child: Text('Login'),
+                    onPressed: () {
+                      print(nameController.text);
+                      print(passwordController.text);
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Home()),
+                      );
+                    },
+                  )),
+              Container(
+                  child: Row(
+                children: <Widget>[
+                  Text(
+                    'Does not have account?',
+                    //style: TextStyle(color:Colors.white),
                   ),
-                ),
-                FlatButton(
-                  onPressed: (){
-                    //forgot password screen
-                  },
-                  textColor: Colors.green,
-                  child: Text('Forgot Password'),
-                ),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.green,
-                      child: Text('Login'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Home()),
-                        );
-                      },
-                    )),
-                Container(
-                    child: Row(
-                      children: <Widget>[
-                        Text('Does not have account?',
-//                        style: TextStyle(color:Colors.white),
-                        ),
-                        FlatButton(
-                          textColor: Colors.green,
-                          child: Text(
-                            'Register an account',
-                            style: TextStyle(fontSize: 15),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => Signup()),
-                            );
-                          },
-                        )
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ))
-              ],
-            )));
+                  FlatButton(
+                    textColor: Colors.green,
+                    child: Text(
+                      'Register an account',
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Signup()),
+                      );
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ))
+            ],
+          )),
+    );
   }
 }
